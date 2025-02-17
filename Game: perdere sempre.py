4bad948224e4751585d206209d1dfb6720b2e33e
Daxcode <daxgiax@gmail.com>
2025-02-17 13:24:15 +0100

    Update Game: perdere sempre.py

--- Game: perdere sempre.py
+++ Game: perdere sempre.py
@@ -1,20 +1,24 @@
+# come perdere sempre al gioco
+
 print("Giochiamo a carta, forbici, sasso?")
 risposta = input()
+
+# con risposta "si", viene eseguito questo codice:
 if risposta == "si":
     print("Giochiamo!")
     print("Mi dici il tuo nome da giocatore?")
-    nome = input()
+    nome = input() # giocatore inserisce il proprio nome
     print(nome + ", allora giochiamo")
     print("\nCosa scegli?")
     scelta_giocatore = ["carta", "forbici", "sasso"]
     for scelta in scelta_giocatore:
         print(scelta)
     print("Scegli 0 per carta, 1 per forbici o 2 per sasso")
-    lettera_scelta = int(input())
+    lettera_scelta = int(input()) # il giocatore inserisce la sua scelta
     scelta_2 = scelta_giocatore[lettera_scelta]
     print("Bene, hai scelto: " + scelta_2)
 
-    scelta_giocatore_b = ""
+    scelta_giocatore_b = "" # il giocatore B sceglierà in base alla scelta del giocatore A e vincerà sempre
     if scelta_2 == "carta":
         scelta_giocatore_b = "forbici"
     elif scelta_2 == "forbici":
@@ -26,5 +30,7 @@ if risposta == "si":
     input()
     print("Io ho scelto: " + scelta_giocatore_b)
     print(nome + ", hai perso!!!")
+
+# con risposta "np" o altro, viene eseguita questa riga:
 else:
     print("Ok, magari un'altra volta!")
